02a91aadb6739153a881845d819e2365533d96f3
anurag0x <anuragkushwah134@gmail.com>
2022-12-10 21:41:13 +0530

    prime problem

---
+++ prime.js
@@ -0,0 +1,10 @@
+sum=0
+    for (i=1; i<43; i++){
+        if (43%i==0){
+           sum+=1 
+        }
+    }if (sum>0){
+        console.log("No")
+    }else if (sum==0){ 
+        console.log("Yes")
+    }
